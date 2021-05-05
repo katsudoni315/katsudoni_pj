1d870c55a0cdc995d5c107b9443b21735436ece0
katsudoni315 <sinobi841@yahoo.co.jp>
2021-05-05 19:52:34 +0900

    天気カードを試験追加

--- components/CommonHeader.vue
+++ components/CommonHeader.vue
@@ -3,14 +3,14 @@
     <b-navbar toggleable="sm" type="dark" variant="info">
       <b-navbar-brand to=“/“>HOME</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
+      <!-- <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/about">about</b-nav-item>
           <b-nav-item to="/game">game</b-nav-item>
           <b-nav-item to="/paint">paint</b-nav-item>
           <b-nav-item to="/scrap">scrap</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
+        </b-navbar-nav>;.
+      </b-collapse> -->
     </b-navbar>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,68 @@
 <template>
-  <section class="container">
-    <b-card class="main-card">
-      <b-card-title>{{ homeTitle }}</b-card-title>
-      <b-card-body>
-        千里の道も一歩より
-      </b-card-body>
-    </b-card>
-  </section>
+  <b-container>
+        <b-row >
+            <b-col>
+              <b-card :img-src="weatherItem.image"
+              overlay>
+                <b-card-header block>
+                  <b-card-title>{{ weatherItem.city }}の天気</b-card-title>
+                </b-card-header>
+                <b-card-body>
+                  <b-img :src="weatherItem.icon"></b-img>
+                <b-card-sub-title class="p-3 bg-info text-white">{{ weatherItem.weather }}</b-card-sub-title>
+                  <b-card-text>
+                    <p>現在の気温:　{{ weatherItem.maxTemp }}℃</p>
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </b-col>
+        </b-row>
+  </b-container>
 </template>
 
 <script>
+import cloudsImage from '@/static/pr00153-640x480.jpg'
 export default {
   data: () => ({  
-    qiitaData: Array,
-    homeTitle: String,
+    qiitaData: [],
+    homeTitle: '',
+    oWobj: {},
+    weatherItem: {
+      city: '',
+      image: '',
+      weather: '',
+      maxTemp: 0,
+      minTemp: 0
+    }
 
   }),
 
-  async mounted() {
-    
-  },
-
-  async created (){
+  async created() {
     this.homeTitle = 'MIURA PAGE'
-    this.qiitaData = JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'),true, '')
+    const apiKey = "5f75aaa751fc6d370dbd9c2744a0e75f"
+    const city = 'Utsunomiya'
+    const iconPass = 'http://openweathermap.org/img/w/'
+
+    this.oWobj = await this.$axios.$get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',jp&units=metric&lang=ja&APPID=' + apiKey)
+    this.weatherItem.city = this.oWobj.city.name
+    
+    this.weatherItem.icon = iconPass + this.oWobj.list[5].weather[0].icon + '.png'
+    this.weatherItem.weather = this.oWobj.list[3].weather[0].description
+    this.weatherItem.minTemp = this.oWobj.list[3].main.temp_min
+    this.weatherItem.maxTemp = this.oWobj.list[3].main.temp_max
+    let imagePass
+    console.log(parseInt(this.oWobj.list[3].weather[0].id))
+    switch (Math.floor(this.oWobj.list[3].weather[0].id / 100)) {
+      case (8):
+        imagePass = cloudsImage
+        break;
+    
+      default:
+        imagePass = '失敗'
+        break;
+    }
+    this.weatherItem.image = imagePass
+
   }
 }
 </script>
